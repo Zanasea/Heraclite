3c248b9dde06852a6cd3ef581feabd9d5caf557e
Jean-Francois Gagnon <jfrancois_gagnon@hotmail.com>
2011-12-22 18:20:24 -0500

    First attempt at network connection

---
+++ src/network/Connection.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package network;
+
+import java.net.Socket;
+
+/**
+ * @author JF
+ * 
+ */
+public interface Connection {
+  public void connect();
+
+  public void disconnect();
+}
---
+++ src/network/ConnectionInformations.java
@@ -0,0 +1,68 @@
+/**
+ * 
+ */
+package network;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+
+/**
+ * @author JF
+ * 
+ */
+public class ConnectionInformations implements Connection {
+  private static final int PORT_NUMBER = 4444;
+
+  private String ipAddress;
+  private String name;
+  private Socket connection;
+
+  public ConnectionInformations(String name, String ipAddress) {
+    this.ipAddress = ipAddress;
+    this.name = name;
+    connection = new Socket();
+  }
+
+  @Override
+  public void connect() {
+    try {
+      connection = new Socket(InetAddress.getByName(ipAddress), PORT_NUMBER);
+    } catch (IOException e) {
+      System.err.println("Impossible d'établir la connexion: " + e.getMessage());
+    }
+  }
+
+  @Override
+  public void disconnect() {
+    try {
+      connection.close();
+    } catch (IOException e) {
+      System.err.println(e.getMessage());
+    }
+  }
+
+  public String getIpAddress() {
+    return ipAddress;
+  }
+
+  public void setIpAddress(String ipAddress) {
+    this.ipAddress = ipAddress;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Socket getConnection() {
+    return connection;
+  }
+
+  public void setConnection(Socket connection) {
+    this.connection = connection;
+  }
+}
