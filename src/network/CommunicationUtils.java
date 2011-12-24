82fe0115bd51ca930627daecf1cb0c811dd3a6f5
Jean-Francois Gagnon <jfrancois_gagnon@hotmail.com>
2011-12-24 00:09:42 -0500

    The communication is now done through Messages

--- src/network/CommunicationUtils.java
+++ src/network/CommunicationUtils.java
@@ -7,17 +7,19 @@ import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 
+import message.Message;
+
 public class CommunicationUtils {
-  public static void sendMessage(Socket socket, Object message) throws IOException {
+  public static void sendMessage(Socket socket, Message message) throws IOException {
     OutputStream stream = socket.getOutputStream();
     ObjectOutputStream oos = new ObjectOutputStream(stream);
     oos.writeObject(message);
     oos.flush();
   }
 
-  public static Object receiveMessage(Socket socket) throws IOException, ClassNotFoundException {
+  public static Message receiveMessage(Socket socket) throws IOException, ClassNotFoundException {
     InputStream stream = socket.getInputStream();
     ObjectInputStream ois = new ObjectInputStream(stream);
-    return ois.readObject();
+    return (Message) ois.readObject();
   }
 }
