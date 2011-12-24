b83c78afc498834e48f6cd72567e4b211fc023b1
Jean-Francois Gagnon <jfrancois_gagnon@hotmail.com>
2011-12-24 00:07:46 -0500

    Moved the logic to the Connection class

--- src/network/Connection.java
+++ src/network/Connection.java
@@ -1,7 +1,41 @@
 package network;
 
-public interface Connection {
-  public void connect();
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
 
-  public void disconnect();
+public class Connection {
+  public static final int PORT_NUMBER = 4444;
+
+  private ConnectionInformations connectionInformations;
+  private Socket socket;
+
+  public Connection(ConnectionInformations connectionInformations) {
+    this.connectionInformations = connectionInformations;
+    socket = new Socket();
+  }
+
+  public void connect() {
+    try {
+      socket = new Socket(InetAddress.getByName(connectionInformations.getIpAddress()), PORT_NUMBER);
+    } catch (IOException e) {
+      System.err.println("Impossible d'établir la connexion: " + e.getMessage());
+    }
+  }
+
+  public void disconnect() {
+    try {
+      socket.close();
+    } catch (IOException e) {
+      System.err.println(e.getMessage());
+    }
+  }
+
+  public Socket getSocket() {
+    return socket;
+  }
+
+  public void setSocket(Socket socket) {
+    this.socket = socket;
+  }
 }
--- src/network/ConnectionInformations.java
+++ src/network/ConnectionInformations.java
@@ -1,39 +1,18 @@
 package network;
 
-import java.io.IOException;
-import java.net.InetAddress;
-import java.net.Socket;
+import java.io.Serializable;
 
-public class ConnectionInformations implements Connection {
-  private static final int PORT_NUMBER = 4444;
-
+public class ConnectionInformations implements Serializable {
+  private static final long serialVersionUID = 497982241148511076L;
+  
   private String ipAddress;
   private String name;
-  private Socket connection;
+  
 
   public ConnectionInformations(String name, String ipAddress) {
     this.ipAddress = ipAddress;
     this.name = name;
-    connection = new Socket();
-  }
-
-  @Override
-  public void connect() {
-    try {
-      connection = new Socket(InetAddress.getByName(ipAddress), PORT_NUMBER);
-    } catch (IOException e) {
-      System.err.println("Impossible d'établir la connexion: " + e.getMessage());
-    }
-  }
-
-  @Override
-  public void disconnect() {
-    try {
-      connection.close();
-    } catch (IOException e) {
-      System.err.println(e.getMessage());
-    }
-  }
+  } 
 
   public String getIpAddress() {
     return ipAddress;
@@ -49,13 +28,5 @@ public class ConnectionInformations implements Connection {
 
   public void setName(String name) {
     this.name = name;
-  }
-
-  public Socket getConnection() {
-    return connection;
-  }
-
-  public void setConnection(Socket connection) {
-    this.connection = connection;
   }
 }
